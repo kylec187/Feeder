5ef60d34b54548afe30f6a68f926bd8f29e01c04
KyleChan <kylec187@nycstudents.net>
2025-09-11 13:20:05 -0400

    Feeder

--- Main.java
+++ Main.java
@@ -2,6 +2,9 @@ public class Main
 {
     public static void main(String[] args)
     {
-        Feeder f = new Feeder();
+        Feeder f = new Feeder(500);
+        System.out.println(f);
+        System.out.println(f.getCurrentFood());
+        System.out.println((int) (Math.random() * 10) + 1);
     }
 }
